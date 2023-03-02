e48d55a939b6ad7cbf224673cd963142e4453e30
farlo0nn <sasha.b0707@gmail.com>
2023-03-02 09:51:05 +0200

    added lessonsTime table template for weekday and schedule table, added lessonstime start_time validator

--- school_bot/db/models.py
+++ school_bot/db/models.py
@@ -1,5 +1,11 @@
+import re
+
+
 from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.orm import validates
 from school_bot.db import base, engine 
+from school_bot.db.errors import TimeValidationError
+from school_bot.logger import logger
 
 class User(base):
     __tablename__ = 'user'
@@ -25,6 +31,50 @@ class Lesson(base):
 
     def __repr__(self) -> str:
         return f"<Lesson: {self.name}>"
+
+
+class LessonStartTime(base):
+    __tablename__ = 'lessonstarttime'
+    id = Column(Integer, primary_key=True)
+    start_time = Column(String, unique=True)
+    
+    @validates('start_time')
+    def startTimeValidator(self, key, start_time):
+        if not isinstance(start_time, str):
+            return False
+        try:
+            splittedTime = re.split(', |\/|:', start_time)
+            if len(splittedTime) != 2:
+                raise TimeValidationError
+            hours, minutes = splittedTime
+            if self.valid(hours) and self.valid(minutes):
+                hours,minutes = int(hours), int(minutes)
+            else:
+                raise TimeValidationError
+            if 0<=hours<=23 and 0<=minutes<=59:
+                return True
+        except TimeValidationError:
+            logger.error("invalid start time data")
+            return False
+
+    @staticmethod
+    def valid(variable):
+        try: 
+            variable = int(variable)
+            return True
+        except ValueError:
+            return False
+
+class WeekDay(base):
+    __tablename__ = 'weekday'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True)
+
+class Schedule(base):
+    __tablename__ = 'schedule'
+    id = Column(Integer, primary_key=True)
+    day_name = Column(String, unique=False, nullable=True)
+    lesson_time = Column()
 
 def createModels():
     base.metadata.create_all(engine)
